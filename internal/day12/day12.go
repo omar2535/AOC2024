0a020c7b9a1075048295e2bbe55b6ff0fffc6dab
Omar2535 <omar@9tsai.ca>
2024-12-12 18:19:32 -0500

    Finish day12 part1

--- internal/day12/day12.go
+++ internal/day12/day12.go
@@ -14,7 +14,7 @@ type Region struct {
 	regionId  string
 	area      int
 	perimeter int
-	positions []Position
+	positions map[Position]bool
 }
 
 func PartOne(isTest bool) {
@@ -22,7 +22,7 @@ func PartOne(isTest bool) {
 	var fileContents []string
 
 	if isTest {
-		fileContents = internal.ReadFileIntoArray("res/day12/day12_example_small.txt")
+		fileContents = internal.ReadFileIntoArray("res/day12/day12_example.txt")
 	} else {
 		fileContents = internal.ReadFileIntoArray("res/day12/day12.txt")
 	}
@@ -42,30 +42,86 @@ func PartOne(isTest bool) {
 		for x := 0; x < len(grid[y]); x++ {
 			if visited[y][x] {
 				continue
+			} else {
+				connectedRegionsMap := make(map[Position]bool)
+				connectedRegions := getConnectedRegions(grid, x, y, grid[y][x], visited)
+				// Update the visited with the regions connected
+				for _, connectedRegion := range connectedRegions {
+					visited[connectedRegion.y][connectedRegion.x] = true
+					connectedRegionsMap[connectedRegion] = true
+				}
+				// Add the regions that we have seen
+				perimeter := computePerimiter(grid, connectedRegionsMap)
+				regions = append(regions, Region{regionId: grid[y][x], area: len(connectedRegions), perimeter: perimeter, positions: connectedRegionsMap})
 			}
 		}
 	}
 
+	// Print the results
+	fmt.Println(regions)
 	fmt.Println(grid)
+	fmt.Println("Total price: ", computePrice(regions))
 }
 
-func getConnectedRegions(grid [][]string, x int, y int, regionId string) []Position {
+func computePrice(regions []Region) int {
+	total := 0
+	for _, region := range regions {
+		total += region.area * region.perimeter
+	}
+	return total
+}
+
+func computePerimiter(grid [][]string, connectedRegionsMap map[Position]bool) int {
+	perimeter := 0
+	for position := range connectedRegionsMap {
+		// Check left if it's the same region
+		if position.x-1 < 0 || !connectedRegionsMap[Position{x: position.x - 1, y: position.y}] {
+			perimeter++
+		}
+
+		// Check right if it's the same region
+		if position.x+1 >= len(grid[position.y]) || !connectedRegionsMap[Position{x: position.x + 1, y: position.y}] {
+			perimeter++
+		}
+
+		// Check up if it's the same region
+		if position.y-1 < 0 || !connectedRegionsMap[Position{x: position.x, y: position.y - 1}] {
+			perimeter++
+		}
+
+		// Check down if it's the same region
+		if position.y+1 >= len(grid) || !connectedRegionsMap[Position{x: position.x, y: position.y + 1}] {
+			perimeter++
+		}
+	}
+	return perimeter
+}
+
+func getConnectedRegions(grid [][]string, x int, y int, regionId string, visited [][]bool) []Position {
 	// Check if the position is out of bounds
 	if x < 0 || y < 0 || y >= len(grid) || x >= len(grid[y]) {
 		return []Position{}
 	}
 
+	if grid[y][x] != regionId {
+		return []Position{}
+	}
+
+	if visited[y][x] {
+		return []Position{}
+	}
 	// Create a list of connected regions
 	connectedRegions := make([]Position, 0)
 
 	// Add the current position
 	connectedRegions = append(connectedRegions, Position{x: x, y: y})
+	visited[y][x] = true
 
 	// Check the 4 directions
-	connectedRegions = append(connectedRegions, getConnectedRegions(grid, x+1, y, regionId)...)
-	connectedRegions = append(connectedRegions, getConnectedRegions(grid, x-1, y, regionId)...)
-	connectedRegions = append(connectedRegions, getConnectedRegions(grid, x, y+1, regionId)...)
-	connectedRegions = append(connectedRegions, getConnectedRegions(grid, x, y-1, regionId)...)
+	connectedRegions = append(connectedRegions, getConnectedRegions(grid, x+1, y, regionId, visited)...)
+	connectedRegions = append(connectedRegions, getConnectedRegions(grid, x-1, y, regionId, visited)...)
+	connectedRegions = append(connectedRegions, getConnectedRegions(grid, x, y+1, regionId, visited)...)
+	connectedRegions = append(connectedRegions, getConnectedRegions(grid, x, y-1, regionId, visited)...)
 
 	return connectedRegions
 }
